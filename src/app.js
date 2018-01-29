34827fd4a55d753109579c5ab521d85eb87544d1
aleesteele <aleesteele@gmail.com>
2018-01-29 13:09:06 -0500

    errors with heroku build

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-import Sidebar from './Sidebar/Sidebar'
+import Sidebar from './Sidebar/Sidebar';
 import './App.css';
 
 class App extends Component {
@@ -16,39 +16,12 @@ class App extends Component {
         const children = React.cloneElement(this.props.children, {})
         return (
             <div className="App">
-                {/* <header className="App-header">
-                    <div className="App-nav">
-                        <div className="App-nav-div">
-                            <Link to="/" className="App-nav-link">Home</Link>
-                        </div>
-                        <div className="App-nav-div">
-                            <Link to="/portfolio" className="App-nav-link">Portfolio</Link>
-                        </div>
-                        <div className="App-nav-div">
-                            <Link to="/about" className="App-nav-link">About</Link>
-                        </div>
-                    </div>
-                </header> */}
                 <header className="App-sidebar">
                     <Sidebar />
                 </header>
                 <div className="App-content">
                     { children }
                 </div>
-                {/* <footer className="App-footer">
-                    <div className="App-foot-div">
-                        <a href="" className="App-footer-link">Facebook</a>
-                    </div>
-                    <div className="App-foot-div">
-                        <a href="" className="App-footer-link">Instagram</a>
-                    </div>
-                    <div className="App-foot-div">
-                        <a href="" className="App-footer-link">Linkedin</a>
-                    </div>
-                    <div className="App-foot-div">
-                        <a href="" className="App-footer-link">Email</a>
-                    </div>
-                </footer> */}
             </div>
         );
     }
