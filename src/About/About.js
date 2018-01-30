29a2f3e3047a32862aaebf06621d2fbd79e7e729
aleesteele <aleesteele@gmail.com>
2018-01-29 22:14:35 -0500

    made font bigger

--- src/About/About.js
+++ src/About/About.js
@@ -24,7 +24,7 @@ class About extends Component {
             </p>
             <p>
                 <u>Notable Projects:</u> <br/>
-                • <i>"Mobility as Multi-Dimensional: Forest Governance at Wangchuck Centennial National Park and Beyond"</i>, final directed research project from semester-long field research program in Bhutan, presented to Royal Government of Bhutan officials and scientists (2017).<br/>
+                • <i>"Mobility as Multi-Dimensional: Forest Governance at Wangchuck Centennial National Park and Beyond"</i>, final directed research project from semester-long field research program in Bhutan, presented to Royal Government of Bhutan officials, researchers and scientists (2017).<br/>
                 • <i>"Economic Empowerment or Environmental Impoverishment? Perspectives on the ‘War on Coal’: A Study on the Black Mesa mines of the Navajo and Hopi Nations"</i>. Analyzed relationship between energy industry and poverty levels on Native American reservations during semester-long project (2016). <br/>
                 • <i>"Qualifying and Quantifying Media Bias: An Investigation into the Differences in Western Media Reportage of Officially-Claimed ISIL Terrorist Attacks From 2014-2016"</i>. Developed 'bias measures’ in reporting in major news outlets for semester long project (2016).<br/>
                 • Investigated effects of mining on land and water bodies for "Expulsions" book by Sociologist Saskia Sassen (2016).<br/>
@@ -32,11 +32,11 @@ class About extends Component {
             </p>
             <p>
                 <u>Select Publications</u><br/>
-                • <i>"Investigating the Global and Local in Wangchuck Centennial National Park: A Case for the Bhutanese Conservation Actors In-Between"</i>. <a href="https://consiliencejournal.org/article/investing-the-global-and-local-in-wangchuck-centennial-national-park-a-case-for-the-bhutanese-conservation-actors-in-between/">Published online in Consilience, the Journal of Sustainable Development, (2018)</a>.<br/>
-                • <i>"Acknowledging the South and Ourselves"</i>. <a href="http://spc.columbiaspectator.com/2013/02/03/acknowledging-south%E2%80%94and-ourselves">Published the Columbia Daily Spectator, (2013)</a>.<br/>
-                • <i>"Moving Pictures"</i>. <a href="http://www.columbiaspectator.com/eye/2013/02/06/moving-pictures/">Published in "The Eye" Magazine, (2013)</a>.<br/>
+                • <i>"Investigating the Global and Local in Wangchuck Centennial National Park: A Case for the Bhutanese Conservation Actors In-Between"</i>. <a href="https://consiliencejournal.org/article/investing-the-global-and-local-in-wangchuck-centennial-national-park-a-case-for-the-bhutanese-conservation-actors-in-between/" className="About-link">Published online in Consilience, the Journal of Sustainable Development, (2018)</a>.<br/>
+                • <i>"Acknowledging the South and Ourselves"</i>. <a href="http://spc.columbiaspectator.com/2013/02/03/acknowledging-south%E2%80%94and-ourselves" className="About-link">Published the Columbia Daily Spectator, (2013)</a>.<br/>
+                • <i>"Moving Pictures"</i>. <a href="http://www.columbiaspectator.com/eye/2013/02/06/moving-pictures/" className="About-link">Published in "The Eye" Magazine, (2013)</a>.<br/>
                 • <i>"On the Venus of Willendorf and My Neighbor"</i>. Published in the print edition of art mag "The Point", Christchurch, NZ (2013).<br/>
-                • <i>Anne Lee Steele Photography</i>. <a href="https://issuu.com/racingminds/docs/may2011/73">Featured in Racing Minds Mag, (2011)</a>.
+                • <i>Anne Lee Steele Photography</i>. <a href="https://issuu.com/racingminds/docs/may2011/73" className="About-link">Featured in Racing Minds Mag, (2011)</a>.
             </p>
             <p>
                 <u>Skills:</u> qualitative & quantitative data analysis, interviewing, literature review, ethnography, field research, mapping<br/>
@@ -65,13 +65,13 @@ class About extends Component {
             <b>WORK EXPERIENCE</b>
             <p>
                 • <u>STAFF INTERN</u> – <i>ACNHUR España/UN High Commissioner for Refugees</i> – Madrid, Spain (2016)<br/>
-                • <u>RESEARCH ASSISTANT</u> – <i>Dr. Saskia Sassen, Sociology Department, Columbia University</i> – New York, USA (2015-2016)<br/>
-                • <u>MEDIA INTERN</u> – <i>Undergraduate Recruitment Committee, Columbia University</i> – New York, USA (2014-2015)<br/>
+                • <u>RESEARCH ASSISTANT</u> – <i>Dr. Saskia Sassen, Sociology Department, Columbia University</i> – New York, USA (2015 - 2016)<br/>
+                • <u>MEDIA INTERN</u> – <i>Undergraduate Recruitment Committee, Columbia University</i> – New York, USA (2014 - 2015)<br/>
                 • <u>SEASONAL VOLUNTEER</u> – <i>Rooftop Films</i> – New York, USA (2014-16)<br/>
-                • <u>SENIOR MENTOR PHOTOGRAPHER</u> – <i>Columbia Photographers Association</i> – New York, USA (2011-2014)<br/>
+                • <u>SENIOR MENTOR PHOTOGRAPHER</u> – <i>Columbia Photographers Association</i> – New York, USA (2011 - 2014)<br/>
                 • <u>PRODUCTION ASSISTANT</u> – <i>WGTN LUX Festival</i> – Wellington, New Zealand (2013)<br/>
                 • <u>CONSERVATION VOLUNTEER</u> – <i>Elephant Valley Project</i> – Mondulkiri, Cambodia (2013)<br/>
-                • <u>ASSOCIATE EDITOR</u> – <i>Halcyon Photography Magazine</i> – New York, USA (2011-2012)<br/>
+                • <u>ASSOCIATE EDITOR</u> – <i>Halcyon Photography Magazine</i> – New York, USA (2011 - 2012)<br/>
                 • <u>FREELANCER</u> – <i>Self-Employed</i> – Hired for portraits, events, branding (2009-Current)
             </p>
 
