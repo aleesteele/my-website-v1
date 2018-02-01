ce9173f09a982f6bf986296017d0bb65d4d020f9
aleesteele <aleesteele@gmail.com>
2018-01-31 21:47:30 -0500

    statement

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -13,9 +13,7 @@ class Contact extends Component {
                 <u><h2 className="Contact-text">
                     I love to learn, meet new people, and am always looking to improve my skills.
                     <br/><br/>
-                    I'm currently looking to grow with a team where I can flex my social-analytical muscles and technical skills. Ideally, this would be somewhere in the realms of technicology, research or media. I'm particularly interested in sustainability, market research, and open data, and am a huge data visualization enthusiast.
-                    <br/><br/>
-                    Feel free to get in touch at <a href="mailto:aleesteele@gmail.com" className="Contact-email">aleesteele@gmail.com</a>.
+                    I'm currently looking to grow with a team where I can flex my social lens, multimedia skills and developer muscles. I'm particularly interested in the realms of technicology, research and media. Feel free to get in touch at <a href="mailto:aleesteele@gmail.com" className="Contact-email">aleesteele@gmail.com</a>.
                 </h2></u>
             </div>
         );
