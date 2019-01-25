17c9698e1ea6cec90a3b92f141d6c18bb54f8283
aleesteele <aleesteele@gmail.com>
2019-01-24 21:33:45 -0500

    update

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,47 +12,21 @@ class Home extends Component {
     render() {
         console.log('inside Home component');
         return (
-            <div>
             <div className="Home">
                 <h1 className="Home-main-text">
-                I studied <a href="https://anthropology.columbia.edu/" className="Home-link link anim-middleout">culture</a> and <a href="http://sociology.columbia.edu/" className="Home-link link anim-middleout">society</a> @ <a href="https://www.columbia.edu/" className="Home-link link anim-middleout">CU</a>, where I focused on <a href="http://www.hup.harvard.edu/catalog.php?isbn=9780674599222" className="Home-link link anim-middleout">sustainable development</a> and <a href="https://eacnur.org/es" className="Home-link link anim-middleout">forced migration</a>. I also <a href="https://consiliencejournal.org/article/investing-the-global-and-local-in-wangchuck-centennial-national-park-a-case-for-the-bhutanese-conservation-actors-in-between/" className="Home-link link anim-middleout">interviewed foresters</a> in the <Link to="/bhutan" className="Home-link link anim-middleout">last Himalayan Kingdom</Link> and <a href="https://youtu.be/9NuDewSvVbA" className="Home-link link anim-middleout">shot a lot of video</a>, amongst <Link to="/other" className="Home-link link anim-middleout">other things</Link>.
-            </h1>
-            <h1 className="Home-main-text">
-                Most recently, I learned <Link to="/spiced" className="Home-link link anim-middleout">how to code</Link>, which has made me a better storyteller and <Link to="/gene" className="Home-link link anim-middleout">web developer</Link>. When I'm not learning how to communicate research for the web, I'm exploring the intersection of geography and technology <a href="https://aleesteele.github.io/" className="Home-link link anim-middleout">here</a>.
-            </h1>
-            <h1 className="Home-main-text">
-              Check out my <a href="http://linkedin.com/in/anneleesteele" className="Home-link link anim-middleout">LinkedIn</a> or social media to learn more about me, or feel free to <a href="mailto:aleesteele@gmail.com" className="Home-link link anim-middleout">get in touch ✉</a>. Thanks! -Anne
+                  Hello, world.<br/>
+                  I'm redoing this little corner of the internet.<br/>
+                  Check back soon. :)
                 </h1>
-                {/* <h1 className="Home-main-text">
-                <a href="http://www.aleesteele.com/Anne-Lee-Steele-CV.pdf" className="Home-link link anim-middleout" download="Anne-Lee-Steele-C18.pdf">CV</a>,
-                    Hello, world.<br/>
-                    I'm redoing this little corner of the internet.<br/>
-                    Check back soon. :)
-               </h1> */}
-                {/* <h1 className="Home-main-text">
-                    Currently: Mul for the <a href="">CSIS iDeas Lab</a>.
-                </h1> */}
                 <div className="App-sidebar-nav-div">
-                    <ol>
-                        <li className="sidebar-icon"><a class="icon-github social-button color" href="http://github.com/aleesteele"></a></li>
-                        <li className="sidebar-icon"><a class="icon-twitter social-button color" href="http://twitter.com/anneleesteele"></a></li>
-                        <li className="sidebar-icon"><a class="icon-instagram social-button color" href="http://instagram.com/mmntsbyanne"></a></li>
-                        <li className="sidebar-icon"><a class="icon-tumblr social-button color" href="https://aleesteele.tumblr.com/"></a></li>
-                        <li className="sidebar-icon"><a class="icon-linkedin social-button color" href="http://linkedin.com/in/anneleesteele"></a></li><br/><br/>
-                    </ol>
+                  <ol>
+                      <li className="sidebar-icon"><a class="icon-github social-button color" href="http://github.com/aleesteele"></a></li>
+                      <li className="sidebar-icon"><a class="icon-twitter social-button color" href="http://twitter.com/anneleesteele"></a></li>
+                      <li className="sidebar-icon"><a class="icon-instagram social-button color" href="http://instagram.com/mmntsbyanne"></a></li>
+                      <li className="sidebar-icon"><a class="icon-linkedin social-button color" href="http://linkedin.com/in/anneleesteele"></a></li>
+                  </ol>
                 </div>
-                {/* <div>
-                <Link to="/video" className="Home-link link anim-middleout"></Link>
-                ↓ <a href='#section1'> See my portfolio </a> ↓
-                </div> */}
             </div>
-        {/* <ScrollableAnchor className="#section1">
-        <Portfolio/>
-        </ScrollableAnchor> */}
-        {/* <ScrollableAnchor className="#section2">
-        hello
-        </ScrollableAnchor> */}
-        </div>
         );
     }
 }
